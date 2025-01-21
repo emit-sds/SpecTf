cf275cf7b5c36b0e1cf7f92838d024c6cf9b1deb
makiper <michael.kiper@jpl.nasa.gov>
2025-01-21 10:19:33 -0800

    Adding Unit Tests

---
+++ tests/train_spectf.py
@@ -0,0 +1,9 @@
+import unittest
+from cloud import train_spectf_cloud as train
+
+class testSpecTfTrain(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_train_spectf(self):
+        pass

bb6a23b26b8eb269bd17fefc77b5b035fda39971
makiper <michael.kiper@jpl.nasa.gov>
2025-02-19 19:06:50 -0800

    updated the arch yaml file and util drop band functions

--- spectf/utils.py
+++ spectf/utils.py
@@ -11,38 +11,43 @@ import os.path as op
 import logging
 import re
 import random
+from typing import List, Optional, Tuple
 
 import numpy as np
 import torch
 
-def seed(i=42):
+def seed(i:int = 42) -> None:
     """ Seed all random number generators """
     random.seed(i)
     np.random.seed(i)
     torch.manual_seed(i)
 
-def verify_dir(dirpath):
+def verify_dir(dirpath:str) -> None:
     """ Verify that the directory path exists, throw if not """
     if not op.isdir(dirpath):
         logging.error("%s does not exist.", dirpath)
         sys.exit(1)
 
-def verify_file(filepath):
+def verify_file(filepath:str) -> None:
     """ Verify that the file path exists, throw if not """
     if not op.isfile(filepath):
         logging.error("%s does not exist.", filepath)
         sys.exit(1)
 
-def name_to_nm(bandname):
+def name_to_nm(bandname:str) -> float:
     """ Convert wavelength text to float """
     return float(re.search(r'(\d+\.\d+)', bandname).group(1))
 
-def get_date(fid):
+def get_date(fid:str) -> str:
     """ Get the date string from full FID """
     return fid.split('_')[0].split('t')[1]
 
-def drop_bands(spectra: np.ndarray, banddef: np.ndarray, wls: list = None,
-               nan: bool = True):
+def drop_bands(
+        spectra: np.ndarray, 
+        banddef: np.ndarray, 
+        drop_wl_ranges: Optional[List[List[int]]] = None,
+        nan: bool = True,
+    ) -> Tuple[np.ndarray, np.ndarray]:
     """
     Removes bands/wavelengths of high uncertainty from a single spectra
     or an array of spectras.
@@ -51,76 +56,53 @@ def drop_bands(spectra: np.ndarray, banddef: np.ndarray, wls: list = None,
     high-uncertainty bands from 1275nm to 1320nm as defined by isofit.
 
     Args:
-        spectra (np.ndarray): The input spectra (1D) or array of spectra (2D).
-        banddef (np.ndarray): The original band wavelengths of the spectra.
-        wls (list, optional): The list of wavelengths to be removed.
-                              Default is defined for EMIT.
-        nan (bool, optional): If True, replace removed bands with NaN.
-                              Default True.
+        spectra (np.ndarray):                       The input spectra (1D) or array of spectra (2D).
+        banddef (np.ndarray):                       The original band wavelengths of the spectra.
+        drop_wl_ranges (List[List[int]], optional): The list of ranges (inclusive) of wavelengths to be removed.
+        nan (bool, optional):                       If True, replace removed bands with NaN.
+                                                    Default True.
 
     Returns:
-        np.ndarray: The modified spectra with bands removed.
-        np.ndarray: The modified band definitions.
+        np.ndarray: The modified spectra with bands removed (either w/ or w/o NaNs).
+        np.ndarray: The modified banddef
     """
-
-    dropbands = []
-
-    if wls is None:
-        wls = [381.0055, 388.4092, 395.8158, 403.2254,
-            1275.339, 1282.794, 1290.25, 1297.705, 1305.16, 1312.614, 1320.068,
-            2455.994, 2463.381, 2470.767, 2478.153, 2485.538, 2492.923]
-
-    for wl in wls:
-        deltas = np.abs(banddef - wl)
-        dropbands.append(np.argmin(deltas))
-
+    mask = np.ones_like(banddef, dtype=bool)
+    for low, high in drop_wl_ranges:
+        mask ^= (banddef >= low) & (banddef <= high)
     if nan:
-        if len(spectra.shape) == 2:
-            spectra[:,dropbands] = np.nan
-        else:
-            spectra[dropbands] = np.nan
+        spectra[..., ~mask] = np.nan
     else:
-        if len(spectra.shape) == 2:
-            spectra = np.delete(spectra, dropbands, axis=1)
-        else:
-            spectra = np.delete(spectra, dropbands, axis=0)
-        banddef = np.delete(banddef, dropbands, axis=0)
+        spectra = np.delete(spectra, ~mask, axis=-1)
+        banddef = banddef[mask]
 
     return spectra, banddef
 
-def drop_banddef(banddef: np.ndarray, wls: list = None):
-    """Removes bands/wavelengths of high uncertainty only from a band
+def drop_banddef(
+        banddef: np.ndarray, 
+        drop_wl_ranges: Optional[List[List[int]]] = None,
+    ) -> np.ndarray:
+    """
+    Removes bands/wavelengths of high uncertainty only from a band
     definition array.
 
-    By default, wavelengths <405nm and >2450nm are removed, as well as the
-    high-uncertainty bands from 1275nm to 1320nm as defined by isofit.
-
     Args:
-        banddef (np.ndarray): The original band definitions.
-        wls (list, optional): The list of wavelengths to be removed.
-                              Default is defined for EMIT.
+        banddef (np.ndarray):                       The original band definitions.
+        drop_wl_ranges (List[List[int]], optional): The list of ranges (inclusive) of wavelengths to be removed.
     
     Returns:
         np.ndarray: The modified band definitions.
     """
-
-    if wls is None:
-        wls = [381.0055, 388.4092, 395.8158, 403.2254,
-            1275.339, 1282.794, 1290.25, 1297.705, 1305.16, 1312.614, 1320.068,
-            2455.994, 2463.381, 2470.767, 2478.153, 2485.538, 2492.923]
-
-    dropbands = []
-    for wl in wls:
-        deltas = np.abs(banddef - wl)
-        dropbands.append(np.argmin(deltas))
-
-    banddef = np.delete(banddef, dropbands, axis=0)
+    mask = np.ones_like(banddef, dtype=bool)
+    for low, high in drop_wl_ranges:
+        mask ^= (banddef >= low) & (banddef <= high)
+    banddef = banddef[mask]
 
     return banddef
 
-def get_device() -> torch.device:
+def get_device(gpu:Optional[int]=None) -> torch.device:
     if torch.cuda.is_available():
-        return torch.device("cuda")
+        c = f"cuda:{gpu}" if gpu else "cuda"
+        return torch.device(c)
     elif torch.backends.mps.is_available() and torch.backends.mps.is_built():
         return torch.device("mps") # Apple silicon
     else:
